6cb29e76e2810f5d637be73e472993d5894b39a8
hane0406 <a0985754375@gmail.com>
2025-03-13 18:52:41 +0800

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>nuk activity</title>
+</head>
+<body bgcolor="black" text="white">
+    <img src="welcome.jpg" width="800">
+    <h1>📢 高雄大學 2025 迎新活動即將登場！</h1>
+    <p>親愛的新同學們：</p>
+    <p>歡迎你加入高雄大學的大家庭！我們精心準備了一場迎新活動，讓你快速適應校園生活，結識新朋友，並了解我們的學校文化！</p>
+    
+    <h2>📆 活動資訊</h2>
+    <ul>
+        <li><b>日期：</b>2025年2月21日</li>
+        <li><b>地點：</b>高雄大學綜合活動中心</li>
+    </ul>
+    
+    <h2>🎉 活動內容</h2>
+    <ul>
+        <li>學校歷史與校園導覽</li>
+        <li>社團與學生組織展示</li>
+        <li>迎新表演與互動遊戲</li>
+        <li>免費小點心 & 抽獎活動</li>
+    </ul>
+    
+    <h2>💡 你將獲得...</h2>
+    <ul>
+        <li>快速適應校園環境，了解重要資源</li>
+        <li>與學長姐交流，獲取學習建議</li>
+        <li>認識來自各地的新朋友</li>
+        <li>參與有趣活動，享受歡樂時光</li>
+    </ul>
+    
+    <p>這將是你大學生活的第一步，快來參加吧！期待在迎新活動見到你！</p>
+    <br>
+    <a href="form.html" style="color: yellow; font-size: 18px;">👉 點此報名迎新活動</a>
+</body>
+</html>
